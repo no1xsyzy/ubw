f249c6d32c3c8bf2a8fb3a102038713a25376c5f
Xu Siyuan <inqb@protonmail.com>
2023-03-12 06:09:35 +0800

    get_danmaku_server

--- src/bilibili/__init__.py
+++ src/bilibili/__init__.py
@@ -4,7 +4,7 @@ from typing import *
 import aiohttp
 from pydantic import parse_obj_as
 
-from bilibili.models import Response, InfoByRoom
+from bilibili.models import Response, InfoByRoom, DanmuInfo
 
 ROOM_INIT_URL = 'https://api.live.bilibili.com/xlive/web-room/v1/index/getInfoByRoom'
 DANMAKU_SERVER_CONF_URL = 'https://api.live.bilibili.com/xlive/web-room/v1/index/getDanmuInfo'
@@ -36,6 +36,16 @@ _Type = TypeVar('_Type')
 @auto_session
 async def get_info_by_room(room_id: int, type_: Type[_Type] = InfoByRoom, *, session: aiohttp.ClientSession) -> _Type:
     async with session.get(ROOM_INIT_URL, params={'room_id': room_id}) as res:
+        data = parse_obj_as(Response[type_], await res.json())
+        if data.code == 0:
+            return data.data
+        else:
+            raise BilibiliApiError(data.message)
+
+
+@auto_session
+async def get_danmaku_server(room_id: int, type_: Type[_Type] = DanmuInfo, *, session: aiohttp.ClientSession) -> _Type:
+    async with session.get(DANMAKU_SERVER_CONF_URL, params={'id': room_id}) as res:
         data = parse_obj_as(Response[type_], await res.json())
         if data.code == 0:
             return data.data
