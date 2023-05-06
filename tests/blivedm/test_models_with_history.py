92171dd323d465e609c9ad3a967254054767a5d3
Xu Siyuan <inqb@protonmail.com>
2023-05-06 09:57:54 +0800

    new(test): test with history

---
+++ tests/blivedm/test_models_with_history.py
@@ -0,0 +1,19 @@
+from json import JSONDecoder
+from pathlib import Path
+
+from pydantic import parse_obj_as
+
+from blivedm import models
+
+decoder = JSONDecoder()
+
+
+def test_with_history():
+    for jsonfile in Path("output/unknown_cmd").glob("*.json"):
+        jsons = jsonfile.read_text('utf-8')
+        idx = 0
+        while idx < len(jsons):
+            json, idx = decoder.raw_decode(jsons, idx)
+            m = parse_obj_as(models.AnnotatedCommandModel, json)
+            c = m.__class__
+            assert models.CommandModel in c.mro() and c is not models.CommandModel
