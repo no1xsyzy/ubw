766db87b74090fc693b4b924d738ebfd5cdbfc4c
Xu Siyuan <inqb@protonmail.com>
2023-05-08 01:03:16 +0800

    update wocpian

--- examples/wocpian.py
+++ examples/wocpian.py
@@ -9,12 +9,17 @@ from rich.markup import escape
 import blivedm
 
 ROOM_IDS = [
+    5252,  # 天堂
     17961,  # 赫萝老师
+    42062,  # 瓶子君152
     81004,  # 艾尔莎_Channel
+    913137,  # 噩梦
     1141542,  # 仙乐阅
     3428783,  # 绫濑光Official
     7688602,  # 花花Haya
     22898905,  # 珞璃Official
+    27299825,  # 布莲雾
+    27337779,  # 月见林林
 ]
 
 logging.basicConfig(
@@ -88,7 +93,7 @@ class PianHandler(blivedm.BaseHandler):
         room_id = client.room_id
 
         if self.maybe_pian(uid, uname) and model.data.msg_type == 1:
-            logger.info(rf"\[[bright_cyan]{room_id}[/]] [bright_red]{uname}[/]进入直播间")
+            logger.info(rf"\[[bright_cyan]{room_id}[/]] [bright_red]{uname}[/]（uid={uid}）进入直播间")
 
     async def on_danmu_msg(self, client, message):
         uid = message.info.uid
@@ -106,6 +111,8 @@ class PianHandler(blivedm.BaseHandler):
 
         if self.maybe_pian(uid, uname):
             logger.info(rf"\[[bright_cyan]{room_id}[/]] 用户 [bright_red]{uname}[/]（uid={uid}）被封禁")
+        else:
+            logger.warning(rf"\[[bright_cyan]{room_id}[/]] 用户 [cyan]{uname}[/]（uid={uid}）被封禁")
 
 
 if __name__ == '__main__':
