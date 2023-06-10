f4417468dc4fb2cf3371b67d4aea9721f5fb2537
Xu Siyuan <inqb@protonmail.com>
2023-06-10 14:09:19 +0800

    fix(model)

--- src/blivedm/models/recommend_card.py
+++ src/blivedm/models/recommend_card.py
@@ -28,49 +28,9 @@ class BtnInfo(BaseModel):
     card_btn_jumpurl: str
 
 
-class ShoppingCardDetail(BaseModel):
-    goods_id: str
-    goods_name: str
-    goods_price: str
-    goods_max_price: str
-    sale_status: int
-    coupon_name: str
-    goods_icon: str
-    goods_status: str
-    source: int
-    h5_url: str
-    jump_link: str
-    schema_url: str
-    is_pre_sale: int
-    activity_info: None
-    pre_sale_info: None
-    early_bird_info: None
-    timestamp: datetime
-    coupon_discount_price: str
-    selling_point: str
-    hot_buy_num: int
-    gift_buy_info: None
-    is_exclusive: bool
-    coupon_id: str
-    reward_info: None
-    goods_tag_list: None
-    virtual_extra_info: VirtualExtraInfo
-    price_info: PriceInfo
-    btn_info: BtnInfo
-    goods_sort_id: int
-    coupon_info: None
-    active_info: None
-    jump_url: str
-
-
-class RecommendData(BaseModel):
-    shopping_card_detail: ShoppingCardDetail
-    recommend_card_extra: None
-
-
 class RecommendCardData(BaseModel):
     title_icon: str
-    recommend_list: list[RecommendData]
+    recommend_list: list[dict]  # 太复杂、一直在变、只是为了收米，谁爱写解析谁写
     timestamp: datetime
 
 
