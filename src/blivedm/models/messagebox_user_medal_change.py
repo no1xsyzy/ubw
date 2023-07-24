6eb6cf7a03591e2705203138c8dc543941a3e30b
Xu Siyuan <inqb@protonmail.com>
2023-07-24 20:49:25 +0800

    feat(model): messagebox_user_medal_change

--- src/blivedm/models/messagebox_user_medal_change.py
+++ src/blivedm/models/messagebox_user_medal_change.py
@@ -11,7 +11,7 @@ class MessageboxUserMedalChangeData(BaseModel):
     medal_level: int
     medal_name: str
     multi_unlock_level: str
-    MultiUnlockLevel: list = Field(default_factory=list)
+    MultiUnlockLevel: list[int] = Field(default_factory=list)
     uid: int
     unlock: int
     unlock_level: int
