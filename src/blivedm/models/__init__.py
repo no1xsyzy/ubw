3f1d26d9a87897840ed6d72950070f2b731da360
Xu Siyuan <inqb@protonmail.com>
2023-09-18 10:44:30 +0800

    new(model): voice_join

--- src/blivedm/models/__init__.py
+++ src/blivedm/models/__init__.py
@@ -97,7 +97,7 @@ from .user_panel_red_alarm import UserPanelRedAlarmCommand
 from .user_task_progress import UserTaskProgressCommand
 from .user_toast_msg import UserToastMsgCommand
 from .video_connection import VideoConnectionMsgCommand, VideoConnectionJoinEndCommand, VideoConnectionJoinStartCommand
-from .voice_join import VoiceJoinStatusCommand
+from .voice_join import VoiceJoinStatusCommand, VoiceJoinListCommand, VoiceJoinRoomCountInfoCommand
 from .warning import WarningCommand
 from .watched_change import WatchedChangeCommand
 from .wealth_notify import WealthNotifyCommand
@@ -198,7 +198,7 @@ AnnotatedCommandModel = Annotated[Union[
     UserTaskProgressCommand,
     UserToastMsgCommand,
     VideoConnectionMsgCommand, VideoConnectionJoinEndCommand, VideoConnectionJoinStartCommand,
-    VoiceJoinStatusCommand,
+    VoiceJoinStatusCommand, VoiceJoinListCommand, VoiceJoinRoomCountInfoCommand,
     WarningCommand,
     WatchedChangeCommand,
     WealthNotifyCommand,
@@ -299,7 +299,7 @@ __all__ = (
     'UserTaskProgressCommand',
     'UserToastMsgCommand',
     'VideoConnectionMsgCommand', 'VideoConnectionJoinEndCommand', 'VideoConnectionJoinStartCommand',
-    'VoiceJoinStatusCommand',
+    'VoiceJoinStatusCommand', 'VoiceJoinListCommand', 'VoiceJoinRoomCountInfoCommand',
     'WarningCommand',
     'WatchedChangeCommand',
     'WealthNotifyCommand',
--- src/blivedm/models/voice_join.py
+++ src/blivedm/models/voice_join.py
@@ -19,3 +19,31 @@ class VoiceJoinStatusCommand(CommandModel):
     cmd: Literal['VOICE_JOIN_STATUS']
     data: VoiceJoinStatusData
     room_id: int
+
+
+class VoiceJoinListData(BaseModel):
+    cmd: str
+    room_id: int
+    category: int
+    apply_count: int
+    red_point: int
+    refresh: int
+
+
+class VoiceJoinListCommand(CommandModel):
+    cmd: Literal['VOICE_JOIN_LIST']
+    data: VoiceJoinListData
+
+
+class VoiceJoinRoomCountInfoData(BaseModel):
+    cmd: str
+    room_id: int
+    root_status: int
+    room_status: int
+    apply_count: int
+    category: int
+
+
+class VoiceJoinRoomCountInfoCommand(CommandModel):
+    cmd: Literal['VOICE_JOIN_ROOM_COUNT_INFO']
+    data: VoiceJoinRoomCountInfoData
