db646056685a561f900e0bd48de6bf1ad5bcba81
Xu Siyuan <inqb@protonmail.com>
2024-01-31 20:39:41 +0800

    test

--- tests/ubw/models/test_models.py
+++ tests/ubw/models/test_models.py
@@ -3,17 +3,22 @@ from pathlib import Path
 
 from pydantic import TypeAdapter
 
-from blivedm import models
+from ubw import models
 
-decoder = JSONDecoder()
+
+def test_exports_not_over():
+    from ubw.models import blive, bilibili
+    assert not set(blive.__all__) & set(bilibili.__all__)
 
 
 def test_with_history():
+    decoder = JSONDecoder()
+    command_adapter = TypeAdapter(models.AnnotatedCommandModel)
     for jsonfile in Path("output/unknown_cmd").glob("*.json"):
         jsons = jsonfile.read_text('utf-8')
         idx = 0
         while idx < len(jsons):
             json, idx = decoder.raw_decode(jsons, idx)
-            m = TypeAdapter(models.AnnotatedCommandModel).validate_python(json)
+            m = command_adapter.validate_python(json)
             c = m.__class__
             assert models.CommandModel in c.mro() and c is not models.CommandModel
