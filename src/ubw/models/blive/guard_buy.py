563dc7583740484391deec5056823437dffaf580
Xu Siyuan <inqb@protonmail.com>
2024-02-05 00:10:24 +0800

    fix(models.guard_buy): fix summarized price

--- src/ubw/models/blive/guard_buy.py
+++ src/ubw/models/blive/guard_buy.py
@@ -2,25 +2,27 @@ from ._base import *
 
 
 class GuardBuyMessage(BaseModel):
-    """上舰消息"""
+    """上舰消息
+    :var uid: 用户ID
+    :var username: 用户名
+    :var guard_level: 舰队等级，0非舰队，1总督，2提督，3舰长
+    :var num: 数量
+    :var price: 单价金瓜子数
+    :var gift_id: 礼物ID
+    :var gift_name: 礼物名
+    :var start_time: 开始时间戳，和结束时间戳相同
+    :var end_time: 结束时间戳，和开始时间戳相同
+    """
+
     uid: int
-    """用户ID"""
     username: str
-    """用户名"""
     guard_level: int
-    """舰队等级，0非舰队，1总督，2提督，3舰长"""
     num: int
-    """数量"""
     price: int
-    """单价金瓜子数"""
     gift_id: int
-    """礼物ID"""
     gift_name: str
-    """礼物名"""
     start_time: datetime
-    """开始时间戳，和结束时间戳相同"""
     end_time: datetime
-    """结束时间戳，和开始时间戳相同"""
 
 
 class GuardBuyCommand(CommandModel):
@@ -33,6 +35,6 @@ class GuardBuyCommand(CommandModel):
             msg=f"{self.data.username} 购买了 {self.data.gift_name}x{self.data.num} (￥{self.data.price})",
             user=(self.data.uid, self.data.username),
             room_id=0,
-            price=self.data.price,
+            price=self.data.price / 1000,
             raw=self,
         )
