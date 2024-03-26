4bb22568bce7811064b2350e6d93984d42bed204
Xu Siyuan <inqb@protonmail.com>
2024-03-26 20:23:19 +0800

    refactor(test)

--- tests/ubw/models/test_models.py
+++ tests/ubw/models/test_models.py
@@ -5,6 +5,8 @@ from pydantic import TypeAdapter
 
 from ubw import models
 
+COMMAND_ADAPTER = TypeAdapter(models.AnnotatedCommandModel)
+
 
 def test_exports_not_over():
     from ubw.models import blive, bilibili
@@ -13,12 +15,11 @@ def test_exports_not_over():
 
 def test_with_history():
     decoder = JSONDecoder()
-    command_adapter = TypeAdapter(models.AnnotatedCommandModel)
     for jsonfile in Path("output/unknown_cmd").glob("*.json"):
         jsons = jsonfile.read_text('utf-8')
         idx = 0
         while idx < len(jsons):
             json, idx = decoder.raw_decode(jsons, idx)
-            m = command_adapter.validate_python(json)
+            m = COMMAND_ADAPTER.validate_python(json)
             c = m.__class__
             assert models.CommandModel in c.mro() and c is not models.CommandModel
