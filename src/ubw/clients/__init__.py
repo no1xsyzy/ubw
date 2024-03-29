da0596d647e29d828eba7893ff748cddac67834e
Xu Siyuan <inqb@protonmail.com>
2024-03-30 00:30:06 +0800

    feat(client): mock client

--- src/ubw/clients/__init__.py
+++ src/ubw/clients/__init__.py
@@ -5,13 +5,14 @@ from pydantic import Field
 from ._livebase import LiveClientABC, HandlerInterface
 from .bilibili import BilibiliCookieClient, BilibiliUnauthorizedClient, BilibiliClientABC, BilibiliClient
 from .openlive import OpenLiveClient
+from .testing import MockClient
 from .wsweb import WSWebCookieLiveClient
 
-LiveClient = Annotated[WSWebCookieLiveClient | OpenLiveClient, Field(discriminator='clientc')]
+LiveClient = Annotated[WSWebCookieLiveClient | OpenLiveClient | MockClient, Field(discriminator='clientc')]
 
 __all__ = (
     'BilibiliCookieClient', 'BilibiliUnauthorizedClient', 'BilibiliClientABC', 'BilibiliClient',
     'LiveClientABC', 'HandlerInterface',
-    'OpenLiveClient', 'WSWebCookieLiveClient',
+    'OpenLiveClient', 'WSWebCookieLiveClient', 'MockClient',
     'LiveClient',
 )
---
+++ src/ubw/clients/testing.py
@@ -0,0 +1,15 @@
+from functools import cached_property
+from unittest.mock import Mock, AsyncMock
+
+from ._livebase import *
+
+
+class MockClient(LiveClientABC):
+    clientc: Literal['mock'] = 'mock'
+
+    close = cached_property(lambda self: AsyncMock(name='close'))
+    join = cached_property(lambda self: AsyncMock(name='join'))
+    start = cached_property(lambda self: Mock(name='start'))
+    stop = cached_property(lambda self: Mock(name='stop'))
+    user_ident = cached_property(lambda self: Mock(name='user_ident'))
+    add_handler = cached_property(lambda self: Mock(name='add_handler'))
