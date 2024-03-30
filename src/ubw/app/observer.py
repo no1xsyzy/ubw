d1e0cf44eac8a29fe2826e5bc7a677f9891a8a67
Xu Siyuan <inqb@protonmail.com>
2024-03-30 22:57:31 +0800

    refactor(app/observer)

--- src/ubw/app/observer.py
+++ src/ubw/app/observer.py
@@ -34,23 +34,29 @@ class ObserverApp(BaseApp):
             self._live_handler = Observer(room_id=(await self.get_room_id()))
         return self._live_handler
 
-    async def _run(self):
-        try:
-            await self.bilibili_client.read_cookie()
+    async def get_live_client(self) -> WSWebCookieLiveClient:
+        if self._live_client is None:
             self._live_client = WSWebCookieLiveClient(
                 room_id=await self.get_room_id(),
                 bilibili_client=self.bilibili_client, bilibili_client_owner=False
             )
+        return self._live_client
+
+    async def _run(self):
+        live_client = None
+        try:
+            await self.bilibili_client.read_cookie()  # checkpoint1
+            live_client = await self.get_live_client()
             live_handler = await self.get_live_handler()
-            self._live_client.add_handler(live_handler)
-            live_handler.start(self._live_client)
-            await live_handler.astart(self._live_client)
-            self._live_client.start()
+            live_client.add_handler(live_handler)
+            live_handler.start(live_client)
+            await live_handler.astart(live_client)  # checkpoint2
+            live_client.start()
 
             # polling dynamics in main
             last_got = set()
             while True:
-                s = await self.bilibili_client.get_user_dynamic(self.uid)
+                s = await self.bilibili_client.get_user_dynamic(self.uid)  # checkpoint3
                 for item in s.items:
                     if item.id_str not in last_got:
                         rich.print(rf"\[{item.pub_date.strftime('%Y-%m-%d %H:%M:%S')}] "
@@ -58,8 +64,14 @@ class ObserverApp(BaseApp):
                                    f" [link {item.jump_url}]链接[/]")
                 last_got = {item.id_str for item in s.items}
                 await asyncio.sleep(self.dynamic_poll_interval)
-        except asyncio.CancelledError:
-            await self._live_client.stop()
+        finally:
+            try:
+                if live_client is not None:
+                    stop = live_client.stop()
+                    if stop is not None:
+                        await stop
+            except asyncio.CancelledError:
+                pass
 
     async def close(self):
         await self._live_client.close()
