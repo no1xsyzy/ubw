712dde1b74359cc421c0390bd53c7bb2e2ff4609
Xu Siyuan <inqb@protonmail.com>
2024-04-07 13:08:39 +0800

    refactor(test): multiple with nested to plain

--- tests/ubw/app/test_observer.py
+++ tests/ubw/app/test_observer.py
@@ -64,45 +64,47 @@ async def test_observer():
 
     bilibili_client.read_cookie.side_effect = read_cookie
 
-    with patch('ubw.app.observer.Observer', spec=Observer) as patch_observer_handler_class:
-
+    with (
+        patch('ubw.app.observer.Observer', spec=Observer) as patch_observer_handler_class,
+        patch('ubw.app.observer.WSWebCookieLiveClient', spec=WSWebCookieLiveClient) as patch_live_client_class,
+        patch('rich.print') as patch_rich_print
+    ):
         async def astart(c):
             checkpoint2.reach()
             assert c is app._live_client
 
         patch_observer_handler_class.return_value.astart.side_effect = astart
-        with patch('ubw.app.observer.WSWebCookieLiveClient', spec=WSWebCookieLiveClient) as patch_live_client_class:
-            async def g():
-                raise asyncio.CancelledError
 
-            patch_live_client_class.return_value.stop.return_value = g()
-            with patch('rich.print') as patch_rich_print:
-                task = asyncio.create_task(p())
+        async def g():
+            raise asyncio.CancelledError
 
-                await checkpoint1
-                bilibili_client.read_cookie.assert_awaited_once_with()
+        patch_live_client_class.return_value.stop.return_value = g()
+        task = asyncio.create_task(p())
 
-                await checkpoint2
-                bilibili_client.get_account_info.assert_awaited_once_with(uid)
-                patch_live_client_class.assert_called_once_with(
-                    room_id=room_id,
-                    bilibili_client=bilibili_client, bilibili_client_owner=False
-                )
-                patch_observer_handler_class.assert_called_once_with(room_id=room_id)
-                live_client: AsyncMock = app._live_client  # noqa
-                live_handler: AsyncMock = app._live_handler  # noqa
-                live_client.add_handler.assert_called_once_with(live_handler)
-                live_handler.start.assert_called_once_with(live_client)
-                live_handler.astart.assert_awaited_once_with(live_client)
+        await checkpoint1
+        bilibili_client.read_cookie.assert_awaited_once_with()
 
-                await checkpoint3
-                live_client.start.assert_called_once_with()
+        await checkpoint2
+        bilibili_client.get_account_info.assert_awaited_once_with(uid)
+        patch_live_client_class.assert_called_once_with(
+            room_id=room_id,
+            bilibili_client=bilibili_client, bilibili_client_owner=False
+        )
+        patch_observer_handler_class.assert_called_once_with(room_id=room_id)
+        live_client: AsyncMock = app._live_client  # noqa
+        live_handler: AsyncMock = app._live_handler  # noqa
+        live_client.add_handler.assert_called_once_with(live_handler)
+        live_handler.start.assert_called_once_with(live_client)
+        live_handler.astart.assert_awaited_once_with(live_client)
 
-                with pytest.raises(asyncio.CancelledError):
-                    await task
-                live_client.stop.assert_called_once_with()
-                patch_rich_print.assert_called_once_with(
-                    r"\[1999-12-31 23:59:59] 发布动态：sample text [link url://fake]链接[/]")
+        await checkpoint3
+        live_client.start.assert_called_once_with()
 
-                await app.close()
-                live_client.close.assert_awaited_once_with()
+        with pytest.raises(asyncio.CancelledError):
+            await task
+        live_client.stop.assert_called_once_with()
+        patch_rich_print.assert_called_once_with(
+            r"\[1999-12-31 23:59:59] 发布动态：sample text [link url://fake]链接[/]")
+
+        await app.close()
+        live_client.close.assert_awaited_once_with()
