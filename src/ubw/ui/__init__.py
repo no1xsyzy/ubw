5746d4d7d41348d4db112ab090e1d91907577919
Xu Siyuan <inqb@protonmail.com>
2024-04-16 13:52:13 +0800

    package(ui): specify __all__

--- src/ubw/ui/__init__.py
+++ src/ubw/ui/__init__.py
@@ -6,4 +6,13 @@ from .live import LiveUI
 from .richy import Richy
 from .web import Web
 
+__all__ = (
+    'UI',
+    'ConsoleUI', 'LiveUI', 'Richy', 'Web',
+    'StreamUI',
+    'Segment',
+    'PlainText', 'Anchor', 'User', 'Room', 'RoomTitle', 'ColorSeeSee', 'DebugInfo', 'Currency', 'Picture',
+    'Record',
+)
+
 UI = Annotated[Union[ConsoleUI, LiveUI, Richy, Web], Field(discriminator='uic')]
