e11e9ce5158b8988d6ea2cce2e45b66895c0faf3
Xu Siyuan <inqb@protonmail.com>
2024-04-29 19:50:05 +0800

    fix(model): two new field

--- src/ubw/models/blive/online_rank_count.py
+++ src/ubw/models/blive/online_rank_count.py
@@ -4,6 +4,8 @@ from ._base import *
 class OnlineRankCountData(BaseModel):
     online_count: int | None = None
     count: int | None = None
+    count_text: str | None = None
+    online_count_text: str | None = None
 
 
 class OnlineRankCountCommand(CommandModel):
