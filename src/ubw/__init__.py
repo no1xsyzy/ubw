f2372485756dfcfbe838394f6cbfa5baf51d46aa
Xu Siyuan <inqb@protonmail.com>
2024-05-08 22:50:19 +0800

    refactor

--- src/ubw/__init__.py
+++ src/ubw/__init__.py
@@ -6,7 +6,7 @@ import sys
 from contextlib import contextmanager
 from enum import Enum
 from pathlib import Path
-from typing import Annotated, Callable, Any
+from typing import Annotated
 
 import typer
 
@@ -275,6 +275,150 @@ class _OutputChoice(str, Enum):
     raw_pretty = 'raw_pretty'
     analyzer = 'analyzer'
     libmpv = 'libmpv'
+    m3u_mpv = 'm3u_mpv'
+
+
+@contextmanager
+def raw(play_info):
+    print(play_info.model_dump_json())
+
+
+@contextmanager
+def raw_pretty(play_info):
+    from rich.pretty import pprint
+    pprint(play_info)
+
+
+@contextmanager
+def info_link(play_info):
+    from rich import get_console
+    console = get_console()
+    from rich.text import Text
+
+    def each(url, info):
+        console.print(Text(info), style=f"link {url}")
+
+    yield each
+
+
+@contextmanager
+def info_link_url(play_info):
+    from rich import get_console
+    console = get_console()
+    from rich.text import Text
+
+    def each(url, info):
+        console.print(Text(f"{info}\t{url}"), style=f"link {url}")
+
+    yield each
+
+
+@contextmanager
+def url_only(play_info):
+    def each(url, info):
+        print(url)
+
+    yield each
+
+
+@contextmanager
+def tsv(play_info):
+    def each(url, info):
+        print('\t'.join([info, *info[1:-1].split("|"), "", url]))
+
+    yield each
+
+
+@contextmanager
+def m3u(play_info):
+    from itertools import count
+    print("#EXTM3U")
+    s = count()
+
+    def each(url, info):
+        print(f"#EXTINF:,[{next(s)}]{info}\n{url}\n")
+
+    yield each
+
+
+@contextmanager
+def m3u_mpv(play_info):
+    from itertools import count
+    import tempfile
+    with tempfile.NamedTemporaryFile(suffix='.m3u', mode='wt', encoding='utf-8') as m3u8file:
+        print("#EXTM3U", file=m3u8file)
+        s = count()
+
+        def each(url, info):
+            print(f"#EXTINF:,[{next(s)}]{info}\n{url}\n", file=m3u8file)
+
+        yield each
+
+        m3u8file.flush()
+
+        returncode = os.spawnl(os.P_WAIT, shutil.which('mpv'),
+                               'mpv', m3u8file.name)
+        print('mpv process exited with code {}'.format(returncode))
+
+
+@contextmanager
+def libmpv(play_info):
+    os.environ["PATH"] = os.path.dirname(__file__) + os.pathsep + os.environ["PATH"]
+    import mpv
+
+    mpv_configs = main.config.get('mpv_configs', {})
+
+    player = mpv.MPV(
+        ytdl=True,
+        input_default_bindings=True, input_vo_keyboard=True, osc=True, osd_font_size=35,
+        **mpv_configs,
+    )
+
+    def each(url, info):
+        print(info, url)
+        player.loadfile(url, 'append-play', title=info, force_media_title=info)
+
+    yield each
+
+    player.wait_for_shutdown()
+
+
+@contextmanager
+def analyzer(play_info):
+    from urllib.parse import urlparse
+    columns = set()
+    records = []
+
+    def each(url, info):
+        scheme, netloc, path, _, query, _ = urlparse(url)
+        r = {'Name': info, 'Host': netloc, 'Url': url}
+        for p in query.split('&'):
+            a, b = p.split('=')
+            if a not in columns:
+                columns.add(a)
+            r[a] = b
+        records.append(r)
+
+    yield each
+
+    import csv
+    writer = csv.DictWriter(sys.stdout, fieldnames=('Name', 'Host', 'Url', *sorted(columns)))
+    writer.writeheader()
+    writer.writerows(records)
+
+
+_OUTPUT_MAP = {
+    _OutputChoice.url_only: url_only,
+    _OutputChoice.info_link: info_link,
+    _OutputChoice.info_link_url: info_link_url,
+    _OutputChoice.tsv: tsv,
+    _OutputChoice.m3u: m3u,
+    _OutputChoice.raw: raw,
+    _OutputChoice.raw_pretty: raw_pretty,
+    _OutputChoice.analyzer: analyzer,
+    _OutputChoice.libmpv: libmpv,
+    _OutputChoice.m3u_mpv: m3u_mpv,
+}
 
 
 def extend_urlinfo(stream, fmt, cdc, url_info, real_original):
@@ -305,137 +449,32 @@ async def get_play_url(room_id: int,
                        print_first: bool = False,
                        real_original: bool = True,
                        ):
-    from rich import get_console
-    from rich.text import Text
-    from .models.bilibili import RoomPlayInfo
-    from .clients.bilibili import BilibiliCookieClient
+    from .clients import BilibiliCookieClient
 
-    console = get_console()
     async with BilibiliCookieClient(**main.config['accounts']['default']) as client:
         play_info = await client.get_room_play_info(room_id, qn)
 
-    each: Callable[[str, str], Any] | None = None
-    after: Callable[[], Any] | None = None
-    empty: Callable[[], Any] | None
+    out_mgr = _OUTPUT_MAP.get(output)
 
-    def empty():
-        print(f'Play info of room {room_id} is empty or filtered', file=sys.stderr)
-
-    match output:
-        case _OutputChoice.raw:
-            print(play_info.json())
-
-        case _OutputChoice.raw_pretty:
-            from rich.pretty import pprint
-            pprint(play_info)
-
-        case _OutputChoice.info_link:
-            def each(url, info):
-                console.print(Text(info), style=f"link {url}")
-
-        case _OutputChoice.info_link_url:
-            def each(url, info):
-                console.print(Text(f"{info}\t{url}"), style=f"link {url}")
-
-        case _OutputChoice.url_only:
-            def each(url, info):
-                print(url)
-
-        case _OutputChoice.tsv:
-            def each(url, info):
-                print('\t'.join([info, *info[1:-1].split("|"), "", url]))
-
-        case _OutputChoice.m3u:
-            from itertools import count
-            print("#EXTM3U")
-            s = count()
-
-            def each(url, info):
-                print(f"#EXTINF:,[{next(s)}]{info}\n{url}\n")
-
-        case _OutputChoice.m3u_mpv:
-            from itertools import count
-            import tempfile
-            m3u8file = tempfile.NamedTemporaryFile(suffix='.m3u', mode='wt', encoding='utf-8')
-            print("#EXTM3U", file=m3u8file)
-            s = count()
-
-            def each(url, info):
-                print(f"#EXTINF:,[{next(s)}]{info}\n{url}\n", file=m3u8file, flush=True)
-
-            def after():
-                returncode = os.spawnl(os.P_WAIT, shutil.which('mpv'), 'mpv', m3u8file.name)
-                print('mpv process exited with code {}'.format(returncode))
-
-        case _OutputChoice.libmpv:
-            # experimental
-            os.environ["PATH"] = os.path.dirname(__file__) + os.pathsep + os.environ["PATH"]
-            import mpv
-
-            mpv_configs = main.config.get('mpv_configs', {})
-
-            player = mpv.MPV(
-                ytdl=True,
-                input_default_bindings=True, input_vo_keyboard=True, osc=True, osd_font_size=35,
-                **mpv_configs,
-            )
-
-            def each(url, info):
-                print(info, url)
-                player.loadfile(url, 'append-play', title=info, force_media_title=info)
-
-            def after():
-                player.wait_for_shutdown()
-
-        case _OutputChoice.analyzer:
-            from urllib.parse import urlparse
-            columns = set()
-            records = []
-
-            def each(url, info):
-                scheme, netloc, path, _, query, _ = urlparse(url)
-                r = {'Name': info, 'Host': netloc, 'Url': url}
-                for p in query.split('&'):
-                    a, b = p.split('=')
-                    if a not in columns:
-                        columns.add(a)
-                    r[a] = b
-                records.append(r)
-
-            def after():
-                import csv
-                writer = csv.DictWriter(sys.stdout, fieldnames=('Name', 'Host', 'Url', *sorted(columns)))
-                writer.writeheader()
-                writer.writerows(records)
-
-        case _:
-            raise NotImplementedError(f'{output} is not implemented')
-
-    if callable(each):
-        each_is_called = False
-        if play_info.playurl_info is not None:
-            for stream in play_info.playurl_info.playurl.stream:
-                if filter_protocol != '' and stream.protocol_name not in filter_protocol:
-                    continue
-                for fmt in stream.format:
-                    if filter_format != '' and fmt.format_name not in filter_format:
+    with out_mgr(play_info) as each:
+        if callable(each):
+            if play_info.playurl_info is not None:
+                for stream in play_info.playurl_info.playurl.stream:
+                    if filter_protocol != '' and stream.protocol_name not in filter_protocol:
                         continue
-                    for cdc in fmt.codec:
-                        if qn != cdc.current_qn:
+                    for fmt in stream.format:
+                        if filter_format != '' and fmt.format_name not in filter_format:
                             continue
-                        if filter_codec != '' and cdc.codec_name not in filter_codec:
-                            continue
-                        for url_info in cdc.url_info:
-                            for u, i in extend_urlinfo(stream, fmt, cdc, url_info, real_original):
-                                each_is_called = True
-                                each(u, i)
-                                if print_first:  # should remove?
-                                    return
-        if not each_is_called and empty is not None:
-            empty()
-
-    if callable(after):
-        after()
+                        for cdc in fmt.codec:
+                            if qn != cdc.current_qn:
+                                continue
+                            if filter_codec != '' and cdc.codec_name not in filter_codec:
+                                continue
+                            for url_info in cdc.url_info:
+                                for u, i in extend_urlinfo(stream, fmt, cdc, url_info, real_original):
+                                    each(u, i)
+                                    if print_first:  # should remove?
+                                        return
 
 
 @app.command()
