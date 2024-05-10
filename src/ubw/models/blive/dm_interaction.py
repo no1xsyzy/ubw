73dde53b3f0e5e6980eecea506e6e06e99a3e236
Xu Siyuan <inqb@protonmail.com>
2024-05-10 19:47:40 +0800

    fix(model): dm_interaction

--- src/ubw/models/blive/dm_interaction.py
+++ src/ubw/models/blive/dm_interaction.py
@@ -19,11 +19,20 @@ class DataData(BaseModel):
     send_interval: int
 
 
+class DataData2(BaseModel):
+    card_appear_interval: int
+    reset_cnt: int = -1
+    display_flag: int = 0
+    cnt: int
+    fade_duration: int
+    suffix_text: str = '人正在点赞'
+
+
 class Data(BaseModel):
     id: int
     status: int
     type: int
-    data: DataData
+    data: DataData | DataData2
     dmscore: int = 0
 
     validate_extra = field_validator('data', mode='before')(strange_dict)
