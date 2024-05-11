7c99dc7bb6101f0aec2f33d01785af06c97adaaa
Xu Siyuan <inqb@protonmail.com>
2024-05-12 01:01:45 +0800

    fix(model): dm_interaction

--- src/ubw/models/blive/dm_interaction.py
+++ src/ubw/models/blive/dm_interaction.py
@@ -12,14 +12,25 @@ class ComboData(BaseModel):
     prefix_icon: str = ''
 
 
-class DataData(BaseModel):
+class Data102Data(BaseModel):
     combo: list[ComboData]
     merge_interval: int
     card_appear_interval: int
     send_interval: int
+    reset_cnt: int = -1
+    display_flag: int = 0
 
 
-class DataData2(BaseModel):
+class Data105Data(BaseModel):
+    fade_duration: int
+    cnt: int
+    card_appear_interval: int
+    suffix_text: str = '人分享了直播间'
+    reset_cnt: int
+    display_flag: int
+
+
+class Data106Data(BaseModel):
     card_appear_interval: int
     reset_cnt: int = -1
     display_flag: int = 0
@@ -32,12 +43,30 @@ class Data(BaseModel):
     id: int
     status: int
     type: int
-    data: DataData | DataData2
     dmscore: int = 0
+
+
+class Data102(Data):
+    type: Literal[102]
+    data: Data102Data
+
+    validate_extra = field_validator('data', mode='before')(strange_dict)
+
+
+class Data105(Data):
+    type: Literal[105]
+    data: Data105Data
+
+    validate_extra = field_validator('data', mode='before')(strange_dict)
+
+
+class Data106(Data):
+    type: Literal[106]
+    data: Data106Data
 
     validate_extra = field_validator('data', mode='before')(strange_dict)
 
 
 class DmInteractionCommand(CommandModel):
     cmd: Literal['DM_INTERACTION']
-    data: Data
+    data: Annotated[Data102 | Data105 | Data106, Field(discriminator='type')]
