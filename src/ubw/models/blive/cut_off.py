3c5f4607ec621599c3062cc224dc2f388dda8ec9
Xu Siyuan <inqb@protonmail.com>
2024-05-13 09:13:39 +0800

    fix(model): cut_off now uses room_id instead of roomid

--- src/ubw/models/blive/cut_off.py
+++ src/ubw/models/blive/cut_off.py
@@ -4,4 +4,4 @@ from ._base import *
 class CutOffCommand(CommandModel):
     cmd: Literal['CUT_OFF']
     msg: str
-    roomid: int
+    room_id: int = Field(validation_alias=AliasChoices('roomid', 'room_id'))
