1b3ec3d8f9be4c74953244dac074b01d1640a24a
Xu Siyuan <inqb@protonmail.com>
2024-05-17 22:46:29 +0800

    fix(model): popular_rank_guide_card

--- src/ubw/models/blive/popular_rank_guide_card.py
+++ src/ubw/models/blive/popular_rank_guide_card.py
@@ -2,7 +2,13 @@ from ._base import *
 
 
 class PopularRankGuideCardData(BaseModel):
-    pass
+    ruid: int
+    title: str
+    sub_text: str = "帮我投喂人气票冲榜吧~"
+    icon_img: str
+    gift_id: int
+    countdown: int
+    popup_title: str = '投喂一个人气票帮助主播打榜~'
 
 
 class PopularRankGuideCardCommand(CommandModel):
