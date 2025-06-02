7d1318590772d87cbe1cd1ec3c71ee8eb9431379
Xu Siyuan <inqb@protonmail.com>
2025-06-02 21:30:35 +0800

    clean

--- src/ubw/actor/__init__.py
+++
@@ -1,9 +0,0 @@
-from typing import Literal
-
-from .actor import BaseActor, InitLoopFinalizeMixin, JustStartOtherActorsMixin
-from .signalslot import Signal, SignalProperty, Slot, SlotProperty, signal_slot
-
-__all__ = (
-    'BaseActor', 'Literal', 'InitLoopFinalizeMixin', 'JustStartOtherActorsMixin',
-    'Signal', 'SignalProperty', 'SlotProperty', 'Slot', 'signal_slot',
-)
